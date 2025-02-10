f2ff131882209984c5c27634f157822013257bc7
Chad Estioco <chadestioco@gmail.com>
2025-02-10 19:08:51 +0100

    Initial code stubs and tests.

--- src/schemanator.py
+++ src/schemanator.py
@@ -1,5 +1,10 @@
 from enum import auto, StrEnum
+import json
 import re
+
+_INT_RE = re.compile(r"\d+")
+_FLOAT_RE = re.compile(r"\d+\.\d+")
+EmanatedSchema = dict[str, "SchemanatedType"]
 
 class SchemanatedType(StrEnum):
     INTEGER = auto()
@@ -7,11 +12,30 @@ class SchemanatedType(StrEnum):
     STRING = auto()
     JSON_OBJECT = "JSONObject"
     JSON_ARRAY = "JSONArray"
+    # Any is only used for fields that take on different types throughout the
+    # given dataset.
     ANY = auto()
 
     @classmethod
     def decide(cls, s):
-        return cls.ANY
+        if _INT_RE.fullmatch(s):
+            return cls.INTEGER
+        elif _FLOAT_RE.fullmatch(s):
+            return cls.FLOAT
+        elif s[0] == "[" and s[-1] == "]":
+            try:
+                parse = json.loads(s)
+                return cls.JSON_ARRAY
+            except json.JSONDecodeError:
+                return cls.STRING
+        elif s[0] == "{" and s[-1] == "}":
+            try:
+                parse = json.loads(s)
+                return cls.JSON_OBJECT
+            except json.JSONDecodeError:
+                return cls.STRING
+
+        return cls.STRING
 
 class Schemanator(object):
 
@@ -26,6 +50,17 @@ class Schemanator(object):
         self.enumerate_union = enumerate_union
 
     def emanate(self, data:list[dict[str, str]]):
+        inferred_schema: EmanatedSchema = {}
         for d in data:
-            for k,v in d:
-                pass                
+            current_schema: EmanatedSchema = {}
+            for k in d:
+                val = d[k]
+                current_schema[k] = SchemanatedType.decide(val)
+
+            inferred_schema = self.__merge_schemas(
+                inferred_schema, current_schema
+            )
+
+    def __merge_schemas(self, s1: EmanatedSchema, s2: EmanatedSchema):
+        # Stub
+        return s1
--- src/tests/schemanator_test.py
+++ src/tests/schemanator_test.py
@@ -5,7 +5,26 @@ from ..schemanator import SchemanatedType
 class SchemanatedTypeTests(unittest.TestCase):
 
     def test_decide(self):
+        self.assertEqual(SchemanatedType.INTEGER, SchemanatedType.decide("1"))
+        # Will not be read as float, like in Java notation
+        self.assertEqual(SchemanatedType.STRING, SchemanatedType.decide("1f"))
+        self.assertEqual(SchemanatedType.FLOAT, SchemanatedType.decide("1.0"))
+        self.assertEqual(SchemanatedType.STRING, SchemanatedType.decide("2.0f"))
+
+        # Only "" and not '' are valid strings in JSON
         self.assertEqual(
-            SchemanatedType.INTEGER,
-            SchemanatedType.decide("1")
+            SchemanatedType.STRING,
+            SchemanatedType.decide("{'data': 'hello'}")
+        )
+        self.assertEqual(
+            SchemanatedType.JSON_OBJECT,
+            SchemanatedType.decide('{"data": "hello"}')
+        )
+        self.assertEqual(
+            SchemanatedType.STRING,
+            SchemanatedType.decide("[1a, 2,3,'4']")
+        )
+        self.assertEqual(
+            SchemanatedType.JSON_ARRAY,
+            SchemanatedType.decide("[1, 2, 3]")
         )
